e56d033f9761fecf2e2dbb38f8b2ea62181cdf1b
MPuzio <martynapuzio93@gmail.com>
2020-03-18 22:18:08 +0100

    styles

--- app/src/views/contactView/contactView.js
+++ app/src/views/contactView/contactView.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import FormImg from "../../images/backgroundForm.jpg";
-import "../contactView/contactView.css";
-// import FormImg2 from "../../images/backgroundImage2.jpg";
-// import FormImg3 from "../../images/image__puppy.jpg";
 import Form from "../../components/form/Form";
 
 class ContactView extends React.Component {
